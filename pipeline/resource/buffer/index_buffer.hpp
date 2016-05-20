0edaa2e8dee183f0e60814a3d630b0bf8d79c4f9
IndignantAngel <180185184@qq.com>
2016-05-20 08:17:08 +0800

    buffers

--- pipeline/resource/buffer/index_buffer.hpp
+++ pipeline/resource/buffer/index_buffer.hpp
@@ -13,105 +13,105 @@ namespace leaves { namespace pipeline
 		}
 	};
 	
-	//class index_buffer : public buffer<index_buffer>
-	//{
-	//public:
-	//	using base_type = buffer<index_buffer>;
-	//	
-	//	template <typename T>
-	//	using iterator = stream_buffer_iterator<T>;
-	//
-	//public:
-	//	// construct
-	//	index_buffer(string&& name, primitive_type primitive, data_format format, size_t primitive_count)
-	//		: base_type(std::move(name), 
-	//			detail::size_of(format), 
-	//			detail::count_of(primitive, primitive_count),
-	//			device_access::none, device_access::read)
-	//		, primitive_(primitive)
-	//		, format_(format)
-	//		, primitive_count_(primitive_count)
-	//	{
-	//		throw_if_invalid_format(format);
-	//		allocate();
-	//	}
-	//
-	//	// attribute access
-	//	primitive_type primitive() const noexcept
-	//	{
-	//		return primitive_;
-	//	}
-	//
-	//	data_format format() const noexcept
-	//	{
-	//		return format_;
-	//	}
-	//
-	//	size_t primitive_count() const noexcept
-	//	{
-	//		return primitive_count_;
-	//	}
-	//
-	//	// attribute modify
-	//	void resize(primitive_type primitive, data_format format, size_t primitive_count)
-	//	{
-	//		throw_if_invalid_format(format);
-	//		base_type::resize(detail::size_of(format), detail::count_of(primitive, primitive_count));
-	//
-	//		primitive_ = primitive;
-	//		format_ = format;
-	//		primitive_count_ = primitive_count;
-	//	}
-	//
-	//	void resize(primitive_type primitive)
-	//	{
-	//		resize(primitive, format_, primitive_count_);
-	//	}
-	//
-	//	void resize(data_format format)
-	//	{
-	//		resize(primitive_, format, primitive_count_);
-	//	}
-	//
-	//	void resize(size_t primitive_count)
-	//	{
-	//		resize(primitive_, format_, primitive_count);
-	//	}
-	//
-	//	template <typename T>
-	//	auto begin()
-	//	{
-	//		static_assert(std::is_integral<T>::value, "Can only be integral type!");
-	//
-	//		if (std::is_signed<T>::value != detail::is_signed(format_) &&
-	//			sizeof(T) != detail::size_of(format_))
-	//			throw std::exception{};
-	//
-	//		return reinterpret_cast<T*>(data());
-	//	}
-	//
-	//	template <typename T>
-	//	auto end()
-	//	{
-	//		static_assert(std::is_integral<T>::value, "Can only be integral type!");
-	//
-	//		if (std::is_signed<T>::value != detail::is_signed(format_) &&
-	//			sizeof(T) != detail::size_of(format_))
-	//			throw std::exception{};
-	//
-	//		return reinterpret_cast<T*>(data() + size());
-	//	}
-	//
-	//private:
-	//	static void throw_if_invalid_format(data_format format)
-	//	{
-	//		if (format != data_format::int_ && format != data_format::uint)
-	//			throw std::exception();
-	//	}
-	//
-	//private:
-	//	primitive_type		primitive_;
-	//	data_format			format_;
-	//	size_t				primitive_count_;
-	//};
+	class index_buffer : public buffer<index_buffer>
+	{
+	public:
+		using base_type = buffer<index_buffer>;
+		
+		template <typename T>
+		using iterator = stream_buffer_iterator<T>;
+	
+	public:
+		// construct
+		index_buffer(string&& name, primitive_type primitive, data_format format, size_t primitive_count)
+			: base_type(std::move(name), 
+				detail::size_of(format), 
+				detail::count_of(primitive, primitive_count),
+				device_access::none, device_access::read)
+			, primitive_(primitive)
+			, format_(format)
+			, primitive_count_(primitive_count)
+		{
+			throw_if_invalid_format(format);
+			allocate();
+		}
+	
+		// attribute access
+		primitive_type primitive() const noexcept
+		{
+			return primitive_;
+		}
+	
+		data_format format() const noexcept
+		{
+			return format_;
+		}
+	
+		size_t primitive_count() const noexcept
+		{
+			return primitive_count_;
+		}
+	
+		// attribute modify
+		void resize(primitive_type primitive, data_format format, size_t primitive_count)
+		{
+			throw_if_invalid_format(format);
+			base_type::resize(detail::size_of(format), detail::count_of(primitive, primitive_count));
+	
+			primitive_ = primitive;
+			format_ = format;
+			primitive_count_ = primitive_count;
+		}
+	
+		void resize(primitive_type primitive)
+		{
+			resize(primitive, format_, primitive_count_);
+		}
+	
+		void resize(data_format format)
+		{
+			resize(primitive_, format, primitive_count_);
+		}
+	
+		void resize(size_t primitive_count)
+		{
+			resize(primitive_, format_, primitive_count);
+		}
+	
+		template <typename T>
+		auto begin()
+		{
+			static_assert(std::is_integral<T>::value, "Can only be integral type!");
+	
+			if (std::is_signed<T>::value != detail::is_signed(format_) &&
+				sizeof(T) != detail::size_of(format_))
+				throw std::exception{};
+	
+			return reinterpret_cast<T*>(data());
+		}
+	
+		template <typename T>
+		auto end()
+		{
+			static_assert(std::is_integral<T>::value, "Can only be integral type!");
+	
+			if (std::is_signed<T>::value != detail::is_signed(format_) &&
+				sizeof(T) != detail::size_of(format_))
+				throw std::exception{};
+	
+			return reinterpret_cast<T*>(data() + size());
+		}
+	
+	private:
+		static void throw_if_invalid_format(data_format format)
+		{
+			if (format != data_format::int_ && format != data_format::uint)
+				throw std::exception();
+		}
+	
+	private:
+		primitive_type		primitive_;
+		data_format			format_;
+		size_t				primitive_count_;
+	};
 } }
--- pipeline/resource/buffer/vertex_buffer.hpp
+++ pipeline/resource/buffer/vertex_buffer.hpp
@@ -6,94 +6,94 @@
 
 namespace leaves { namespace pipeline
 {
-	//template <>
-	//struct buffer_traits<vertex_buffer>
-	//{
-	//	static constexpr object_type type() noexcept
-	//	{
-	//		return object_type::vertex_buffer;
-	//	}
-	//};
-	//
-	//class vertex_buffer : public buffer<vertex_buffer>
-	//{
-	//public:
-	//	using base_type = buffer<vertex_buffer>;
-	//
-	//	template <typename T>
-	//	using iterator = stream_buffer_iterator<T>;
-	//
-	//public:
-	//	// constructor
-	//	vertex_buffer(string&& name, input_layout&& layout, size_t count)
-	//		: base_type(std::move(name), layout.size(), count, device_access::none, device_access::read)
-	//		, layout_(std::move(layout))
-	//	{
-	//		allocate();
-	//	}
-	//
-	//	// attribute access
-	//	input_layout const& layout() const noexcept
-	//	{
-	//		return layout_;
-	//	}
-	//
-	//	// attribute modify
-	//	void resize(input_layout&& meta_data, size_t count)
-	//	{
-	//		auto elem_size = meta_data.size();
-	//		base_type::resize(elem_size, count);
-	//		layout_ = std::move(meta_data);
-	//	}
-	//
-	//	// content access
-	//	template <typename T>
-	//	auto begin(data_semantic semantic)
-	//	{
-	//		auto& attr = layout_.find(semantic);
-	//
-	//		if (sizeof(T) != detail::size_of(attr.format))
-	//			throw std::exception{};
-	//
-	//		using iterator_type = iterator<T>;
-	//		return iterator_type{ data(), data() + size(), attr.offset, static_cast<std::ptrdiff_t>(elem_size()) };
-	//	}
-	//
-	//	template <typename T>
-	//	auto end(data_semantic semantic)
-	//	{
-	//		auto& attr = layout_.find(semantic);
-	//
-	//		if (sizeof(T) != detail::size_of(attr.format))
-	//			throw std::exception{};
-	//
-	//		using iterator_type = iterator<T>;
-	//		auto ptr = data() + size();
-	//		return iterator_type{ ptr, ptr, attr.offset, static_cast<std::ptrdiff_t>(elem_size()) };
-	//	}
-	//
-	//	template <typename T>
-	//	auto begin()
-	//	{
-	//		if(sizeof(T) != elem_size())
-	//			throw std::exception{};
-	//
-	//		using iterator_type = iterator<T>;
-	//		return iterator_type{ data(), data() + size(), 0, sizeof(T) };
-	//	}
-	//
-	//	template <typename T>
-	//	auto end()
-	//	{
-	//		if (sizeof(T) != elem_size())
-	//			throw std::exception{};
-	//
-	//		using iterator_type = iterator<T>;
-	//		auto ptr = data() + size();
-	//		return iterator_type{ ptr, ptr, 0, sizeof(T) };
-	//	}
-	//
-	//private:
-	//	input_layout	layout_;
-	//};
+	template <>
+	struct buffer_traits<vertex_buffer>
+	{
+		static constexpr object_type type() noexcept
+		{
+			return object_type::vertex_buffer;
+		}
+	};
+	
+	class vertex_buffer : public buffer<vertex_buffer>
+	{
+	public:
+		using base_type = buffer<vertex_buffer>;
+	
+		template <typename T>
+		using iterator = stream_buffer_iterator<T>;
+	
+	public:
+		// constructor
+		vertex_buffer(string&& name, input_layout&& layout, size_t count)
+			: base_type(std::move(name), layout.size(), count, device_access::none, device_access::read)
+			, layout_(std::move(layout))
+		{
+			allocate();
+		}
+	
+		// attribute access
+		input_layout const& layout() const noexcept
+		{
+			return layout_;
+		}
+	
+		// attribute modify
+		void resize(input_layout&& meta_data, size_t count)
+		{
+			auto elem_size = meta_data.size();
+			base_type::resize(elem_size, count);
+			layout_ = std::move(meta_data);
+		}
+	
+		// content access
+		template <typename T>
+		auto begin(data_semantic semantic)
+		{
+			auto& attr = layout_.find(semantic);
+	
+			if (sizeof(T) != detail::size_of(attr.format))
+				throw std::exception{};
+	
+			using iterator_type = iterator<T>;
+			return iterator_type{ data(), data() + size(), attr.offset, static_cast<std::ptrdiff_t>(elem_size()) };
+		}
+	
+		template <typename T>
+		auto end(data_semantic semantic)
+		{
+			auto& attr = layout_.find(semantic);
+	
+			if (sizeof(T) != detail::size_of(attr.format))
+				throw std::exception{};
+	
+			using iterator_type = iterator<T>;
+			auto ptr = data() + size();
+			return iterator_type{ ptr, ptr, attr.offset, static_cast<std::ptrdiff_t>(elem_size()) };
+		}
+	
+		template <typename T>
+		auto begin()
+		{
+			if(sizeof(T) != elem_size())
+				throw std::exception{};
+	
+			using iterator_type = iterator<T>;
+			return iterator_type{ data(), data() + size(), 0, sizeof(T) };
+		}
+	
+		template <typename T>
+		auto end()
+		{
+			if (sizeof(T) != elem_size())
+				throw std::exception{};
+	
+			using iterator_type = iterator<T>;
+			auto ptr = data() + size();
+			return iterator_type{ ptr, ptr, 0, sizeof(T) };
+		}
+	
+	private:
+		input_layout	layout_;
+	};
 } }
